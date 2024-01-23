62a48d6f3c319be0d2444947085f575f4d835fb6
Reed Searle <csearle@cnm.edu>
2024-01-23 15:01:53 -0700

    Initial commit with .evaluate returning concatenated strings

---
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive.fizzbuzz;
+
+import edu.cnm.deepdive.fizzbuzz.service.FizzBuzz;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO Invoke main loop to print fizzbuzz values & numbers.
+
+    for (int counter = 1; counter <= 100; counter++) {
+      String value = FizzBuzz.evaluate(counter);
+      System.out.printf("%s ", value);
+//      System.out.printf("%s%n", FizzBuzz.evaluate(counter));
+      if (value.contentEquals("fizz buzz")){
+        System.out.printf("%n");
+      }
+    }
+  }
+}
